152064a8a06009ff894ddfd46f830eb683a358a9
Andersmb <anders.brakestad@gmail.com>
2018-11-15 14:34:33 +0100

    script now writes a file containing some raw data for each job analylzed, and prints in a nicer format the data necessary for comparing FD dipole moments with the internal ones

--- mrchem_analysis.py
+++ mrchem_analysis.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 # Get all relevant output files in a list
 files = glob.glob("*_*_*_*_.out")
+nullfield_files = filter(lambda f: f.split("_")[3] == "00", files)
 error_files = filter(lambda f: not MrchemOut(f).normaltermination(), files)
 
 #Test if all jobs terminated normally
@@ -15,35 +16,79 @@ assert len(error_files) == 0, "Error! These {} job(s) did not terminate normally
 print("All jobs terminated normally")
 
 # now we construct the dict and fill with information from filenames
-data = {}
-data["molecule"] =    [f.split("_")[0] for f in files if f not in error_files]
-data["functional"] = [f.split("_")[1] for f in files if f not in error_files]
-data["field"] =       [f.split("_")[3] for f in files if f not in error_files]
-data["energy"] = []
-data["dipole"] = []
-data["filename"] = []
-data["precision"] = []
+# this dict will contain the raw data for each calculation
+rawdata = {}
+rawdata["molecule"] =    [f.split("_")[0] for f in files]
+rawdata["functional"] = [f.split("_")[1] for f in files]
+rawdata["field"] =       [f.split("_")[3] for f in files]
+rawdata["energy"] = []
+rawdata["dipole"] = []
+rawdata["filename"] = []
+rawdata["precision"] = []
 
 # now collect the energies, dipoles, filenames, and precisions from output 
 # and add them to the dict
 for f in files:
     output = MrchemOut(f)
-    if f not in error_files:
-        data["energy"].append(output.energy_pot())
-        data["dipole"].append(output.dipole_au())
-        data["filename"].append(output.filename)
-        data["precision"].append(output.precision())
+    rawdata["energy"].append(output.energy_pot())
+    rawdata["dipole"].append(output.dipole_au())
+    rawdata["filename"].append(output.filename)
+    rawdata["precision"].append(output.precision())
     
 # now write raw data to CSV file using a useful pandas command
-pd.DataFrame(data).to_csv("rawdata.csv")
+pd.DataFrame(rawdata).to_csv("rawdata.csv")
 
-# to minimize manual manipulation in Excel, we should transform the raw data to the following format:
 
-# molecule   field_strength    E^+     E^-     dipole_ref
+# We will need a function that converts a string into a float. Example: "00025" -> 0.0025
+def decimal(s):
+    return s[0:1] + "." + s[1:]
 
-# this means we need to pair up certain files. For instance, all jobs with +-field_strengths must be paired
-# and the corresponding dipole_ref also need to be there. Thus we need to construct "triplets" of files:
+# now collect the jobs on same molecule with same functional at same precision, but with fields of opposite signs
+# appending filenames and absolute value of field strength
+triplet = []
+for f in filter(lambda f: f.split("_")[3] == "-001", files):
+    for g in filter(lambda f: f.split("_")[3] == "+001", files):
+        if f.split("_")[0] == g.split("_")[0] and f.split("_")[1] == g.split("_")[1] and f.split("_")[2] == g.split("_")[2]:
+            triplet.append([f, g, decimal(f.split("_")[3][1:])])
 
-# triplet = [(positive field, negative_field, dipole_ref), (triplet2), (triplet3), ..., (triplet_n)]
+for f in filter(lambda f: f.split("_")[3] == "-0005", files):
+    for g in filter(lambda f: f.split("_")[3] == "+0005", files):
+        if f.split("_")[0] == g.split("_")[0] and f.split("_")[1] == g.split("_")[1] and f.split("_")[2] == g.split("_")[2]:
+            triplet.append([f, g, decimal(f.split("_")[3][1:])])
+
+for f in filter(lambda f: f.split("_")[3] == "-00025", files):
+    for g in filter(lambda f: f.split("_")[3] == "+00025", files):
+        if f.split("_")[0] == g.split("_")[0] and f.split("_")[1] == g.split("_")[1] and f.split("_")[2] == g.split("_")[2]:
+            triplet.append([f, g, decimal(f.split("_")[3][1:])])
+
+
+# now we need to append the appropriate null field files to each sublist along with molecule, functional, and precision
+for f in nullfield_files:
+    for trip in triplet:
+        if f.split("_")[0] == trip[0].split("_")[0] and f.split("_")[1] == trip[0].split("_")[1] and f.split("_")[2] == trip[0].split("_")[2]:
+            trip.append(f)
+            trip.append(f.split("_")[0])
+            trip.append(f.split("_")[1])
+            trip.append(str(MrchemOut(f).precision()))
+
+
+# first unzip the sorted list (the order has been triple checked)
+minus, plus, field, null, mol, func, prec = (zip(*sorted(triplet)))
+
+# then map the energy/diple moment to the list, and convert to string
+plus = map(str, map(lambda f: MrchemOut(f).energy_pot(), plus))
+minus = map(str, map(lambda f: MrchemOut(f).energy_pot(), minus))
+null = map(str, map(lambda f: MrchemOut(f).dipole_au(), null))
+
+# then zip back
+triplet = zip(mol, func, prec, field, plus, minus, null)
+
+# now insert header
+triplet.insert(0, ("molecule", "functional", "precision", "field_strength", "e+", "e-", "dipole_null"))
+
+# finally print to terminal in a format easily copied to Excel
+for i in triplet:
+    print(' '.join(i))
+
 
 
