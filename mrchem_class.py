41bb3cc254e5827d4dc82589614a983434ab9fa9
Andersmb <anders.brakestad@gmail.com>
2018-11-20 11:43:51 +0100

    made content into generator, implemented no_scfcycles, scf_energy, and plot_scf_energy

--- mrchem_class.py
+++ mrchem_class.py
@@ -1,21 +1,23 @@
 #! /usr/bin/env python
 import sys
-
+import matplotlib.pyplot as plt
 
 # Define the class MrchemOut, which will be MRChem output files.
 class MrchemOut(object):
     def __init__(self, filename):
-        """self.content returns a list of strings where each string
-        is a line in the original file."""
         self.filename = filename
+
+    def content(self):
+        """Return a generator that yields the lines of the output file"""
         with open(self.filename, "r") as f:
-            self.content = f.readlines()
+            while True:
+                yield f.next()
 
     def normaltermination(self):
         """This method evaluates whether the job
         terminated normally, and returns a Boolean:
         True if termination was notmal, False if not."""
-        if "Exiting MRChem" in self.content[-7]:
+        if "Exiting MRChem" in list(self.content())[-7]:
             return True
         return False
 
@@ -23,10 +25,10 @@ class MrchemOut(object):
     def dipole_debye(self):
         """This method returns the calculated
         dipole moment in Debye (float)"""
-        output = self.content
+        output = list(self.content())
         dipmom = None
 
-        for i,line in enumerate(output):
+        for i,line in enumerate(self.content()):
             if line.strip().startswith("Length of vector"):
                 dipmom = output[i+1]
 
@@ -35,27 +37,26 @@ class MrchemOut(object):
     def dipole_au(self):
         """This method returns the calculated
         dipole moment in atomic units (float)"""
-        output = self.content
+        output = list(self.content())
         dipmom = None
 
-        for i,line in enumerate(output):
+        for i,line in enumerate(self.content()):
             if line.strip().startswith("Length of vector"):
-                dipmom = output[i]
+                dipmom = float(output[i].split()[-1])
 
-        return float(dipmom.split()[-1])
+        return dipmom
 
-    def energy_pot(self):
+    def final_energy_pot(self):
         """This method returns the optimized potential energy (float)"""
-        output = self.content
         e = None
-        for line in output:
+        for line in self.content():
             if ' '.join(line.split()).startswith("Total energy (au)"):
                 e = float(line.split()[-1].strip())
         return e
 
     def precision(self):
         """This method returns the multiwavelet precision used in the calculation (float)"""
-        output = self.content
+        output = self.content()
         prec = None
         for line in output:
             if line.strip().startswith("Current precision"):
@@ -65,13 +66,21 @@ class MrchemOut(object):
 
     def no_scfcycles(self):
         """This method returns the number of SCF cycles performed before convergence (float)"""
-        pass
+        return len(filter(lambda x: "SCF cycle" in x, self.content()))
+
+    def scf_energy(self):
+        """Return a list of floats containing the SCF energies"""
+        e = filter(lambda x: x.strip().startswith("Total energy"), self.content())
+        return map(float, map(lambda x: x.strip().split()[-1], e))
+
+    def plot_scf_energy(self):
+        """Return a graph plotting the potential energies"""
+        return plt.show(plt.plot(self.scf_energy()))
 
     def walltime(self):
         """This function returns the total walltime for the job (float) in seconds"""
-        output = self.content
         w = None
-        for line in output:
+        for line in self.content():
             if ' '.join(line.strip().split()).startswith("*** Wall time"):
                 w_tot = float(line.strip().split()[3])
         return w
