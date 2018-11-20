610163732e27707cec90d8a175c3fa78fc3c6d18
Andersmb <anders.brakestad@gmail.com>
2018-11-20 16:01:15 +0100

    Added decorator function for timing

--- Gaussian.py
+++ Gaussian.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import time
 
 
 # Define the class GaussianOut, which will be Gaussian output files.
@@ -6,50 +7,71 @@ class GaussianOut(object):
     def __init__(self, filename):
         self.filename = filename
 
+    # Timing decorator function
+    def timeit(method):
+        """This function will act as a decorator so we can time all functions"""
+        def timed(*args, **kwargs):
+            before = time.time()
+            result = method(*args, **kwargs)
+            after = time.time()
+    
+            print("Elapsed time: {} seconds".format(after - before))
+    
+            return result
+        return timed
+
+    @timeit
     def __repr__(self):
         return "<GaussianOut(filename={})>".format(self.filename)
 
+    @timeit
     def content(self):
         """Return a generator that iterates over lines in the file, one by one."""
         with open(self.filename, "r") as f:
             while True:
                 yield f.next()
     
+    @timeit
     def source(self):
         """Return the file content as a string."""
         with open(self.filename, "r") as f:
             return f.read()
-
+    
+    @timeit
     def normaltermination(self):
         """Evaluate whether the job
         terminated normally, and return a Boolean:
         True if termination was normal, False if not."""
-        if self.content()[-1].strip().startswith("Normal termination"):
+        if list(self.content())[-1].strip().startswith("Normal termination"):
             return True
         return False
-
+    
+    @timeit
     def scf_energy(self):
         """Return a list of floats containing the optimized SCF energies"""
         energies = filter(lambda x: x.strip().startswith("SCF Done:"), self.content())
         return map(lambda x: float(x.split()[4]), energies)
 
+    @timeit
     def no_scfcycles(self):
         """Return a list of floats containing the number of SCF iterations needed for converging each geom step"""
         cycles = filter(lambda x: x.strip().startswith("SCF Done:"), self.content())
         return map(int, map(lambda x: x.strip().split()[7], cycles))
 
+    @timeit
     def walltime(self):
         """Return the total walltime for the job (float) in seconds"""
         w = filter(lambda x: x.strip().startswith("Elapsed time:"), self.content())[0].strip().split()
         return float(w[2])*24*60*60 + float(w[4])*60*60 + float(w[6])*60 + float(w[8])
 
-
+    @timeit
     def no_atoms(self):
         """Return the number of atoms of the system (integer)"""
         for line in self.content():
             if line.strip().startswith("NAtoms="):
                 return int(line.strip().split()[1])
 
+    @timeit
     def geometry_trajectory(self):
         """Return list of all geometry steps from a geometry optimization. The last step is the optimized geometry"""
         natoms = self.no_atoms()
@@ -90,11 +112,13 @@ class GaussianOut(object):
         
         # We discard the last geometry because it will be a duplicate
         return traj[:-1]
-
+    
+    @timeit
     def no_geomcycles(self):
         """Return the number of geometry cycles needed for convergence. Return an integer."""
         return len(list(self.geometry_trajectory()))
-
+    
+    @timeit
     def no_basisfunctions(self):
         """Return the number of basis functions (integer)."""
         nbasis = None
@@ -104,30 +128,35 @@ class GaussianOut(object):
                 break
         return nbasis
 
+    @timeit
     def maxforce(self):
         """Return a list of floats containing all Max Forces for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("Maximum Force"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
 
+    @timeit
     def rmsforce(self):
         """Return a list of floats containing all RMS Forces for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("RMS Force"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
 
+    @timeit
     def maxstep(self):
         """Return a list of floats containing all Max Steps for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("Maximum Displacement"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
 
+    @timeit
     def rmsstep(self):
         """Return a list of floats containing all RMS Steps for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("RMS Displacement"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
-
+    
+    @timeit
     def tol_maxforce(self):
         """Return the Max Force convergence tolerance as float"""
         t = None
@@ -137,6 +166,7 @@ class GaussianOut(object):
                 break
         return t
 
+    @timeit
     def tol_rmsforce(self):
         """Return the RMSD Force convergence tolerance as float"""
         t = None
@@ -146,6 +176,7 @@ class GaussianOut(object):
                 break
         return t
 
+    @timeit
     def tol_maxstep(self):
         """Return the Max Step convergence tolerance as float"""
         t = None
@@ -155,6 +186,7 @@ class GaussianOut(object):
                 break
         return t
 
+    @timeit
     def tol_rmsstep(self):
         """Return the RMSD Step convergence tolerance as float"""
         t = None
@@ -175,10 +207,4 @@ class GaussianIn(object):
 
     def linkzero(self):
         pass
-
-
-
-
-
-
 
