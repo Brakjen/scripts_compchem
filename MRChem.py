bbdfde409856529cbe00ccb178c21f92924311ca
Andersmb <anders.brakestad@gmail.com>
2018-11-21 13:25:38 +0100

    added decorator for timing

--- MRChem.py
+++ MRChem.py
@@ -6,7 +6,20 @@ import matplotlib.pyplot as plt
 class MrchemOut(object):
     def __init__(self, filename):
         self.filename = filename
-
+    
+    # Timing decorator function
+    def timeit(method):
+        """This function will act as a decorator so we can time all functions"""
+        def timed(*args, **kwargs):
+            before = time.time()
+            result = method(*args, **kwargs)
+            after = time.time()
+    
+            print("Elapsed time: {} seconds".format(after - before))
+    
+            return result
+        return timed
+    @timeit
     def content(self):
         """Return a generator that yields the lines of the output file"""
         with open(self.filename, "r") as f:
@@ -22,6 +35,7 @@ class MrchemOut(object):
         return False
 
 
+    @timeit
     def dipole_debye(self):
         """This method returns the calculated
         dipole moment in Debye (float)"""
@@ -34,6 +48,7 @@ class MrchemOut(object):
 
         return float(dipmom.split()[-1])
     
+    @timeit
     def dipole_au(self):
         """This method returns the calculated
         dipole moment in atomic units (float)"""
@@ -46,6 +61,7 @@ class MrchemOut(object):
 
         return dipmom
 
+    @timeit
     def final_energy_pot(self):
         """This method returns the optimized potential energy (float)"""
         e = None
@@ -53,7 +69,8 @@ class MrchemOut(object):
             if ' '.join(line.split()).startswith("Total energy (au)"):
                 e = float(line.split()[-1].strip())
         return e
-
+    
+    @timeit
     def precision(self):
         """This method returns the multiwavelet precision used in the calculation (float)"""
         output = self.content()
@@ -64,19 +81,23 @@ class MrchemOut(object):
                 break
         return prec
 
+    @timeit
     def no_scfcycles(self):
         """This method returns the number of SCF cycles performed before convergence (float)"""
         return len(filter(lambda x: "SCF cycle" in x, self.content()))
 
+    @timeit
     def scf_energy(self):
         """Return a list of floats containing the SCF energies"""
         e = filter(lambda x: x.strip().startswith("Total energy"), self.content())
         return map(float, map(lambda x: x.strip().split()[-1], e))
 
+    @timeit
     def plot_scf_energy(self):
         """Return a graph plotting the potential energies"""
         return plt.show(plt.plot(self.scf_energy()))
 
+    @timeit
     def walltime(self):
         """This function returns the total walltime for the job (float) in seconds"""
         w = None
