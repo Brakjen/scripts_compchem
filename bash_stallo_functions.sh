d92593cf2ed9a2ff8e48d5166e71a7d4ccc7bf6b
Andersmb <anders.brakestad@gmail.com>
2018-11-23 14:24:40 +0100

    added comments, removed unused functions

--- bash_stallo_functions.sh
+++ bash_stallo_functions.sh
@@ -1,3 +1,5 @@
+
+# print SLURM queue
 function q() {
 NOJOBS=$(bc <<< "$(squeue -u ambr | wc -l) - 1")
 NORUNNING=$(squeue -u ambr | awk '$5 == "R" {print $5}' | wc -l)
@@ -17,6 +19,9 @@ function qcl() {
 	clear
 	q
 }
+
+# print the job history. By default shows history from jobs started today. Optional argument for showing
+# older jobs. Give date in format yyyy-mm-dd
 function jh() {
 TODAY=$1
 if [ "$1" == "" ];then
@@ -32,6 +37,8 @@ sacct --starttime $TODAY --format=User%4,JobID,Jobname%50,state,time,nnodes%2,nc
 function bashsource() {
 source ~/.bashrc
 }
+
+# Same as above, but clear terminal windows first
 function jhc() {
 TODAY=$1
 if [ "$1" == "" ];then
@@ -45,10 +52,12 @@ echo "-------------------------------------------------------"
 sacct --starttime $TODAY --format=User%4,JobID,Jobname%50,state,time,nnodes%2,ncpus%3,CPUTime,elapsed,Start | grep "ambr"
 }
 
+# Move files to Trash directory in home instead of removing
 function rmm() {
 mv "$@" ~/Trash/
 }
 
+# Empty Trash directory
 function emptytrash() {
 trashdir=~/Trash
 if [ -d $trashdir ]; then
@@ -63,6 +72,7 @@ else
 fi
 }
 
+# Print full path to important files in scratch directory of a particular PID.
 function wd() {
 JOBID=$1
 find /global/work/ambr/*${JOBID}* -iname "*.out"
@@ -71,39 +81,28 @@ find /global/work/ambr/*${JOBID}* -iname "*.chk"
 
 }
 
+# Show full path to files matching a pattern. Useful for getting paths to files from queue, where
+# only part of the file name is shown
 function sf() {
 NAME=$1
 find /home/ambr/* -iname "$1*"
 }
 
+# Go to the directory containing the specfied file (give absolute path). Useful for quickly cd-ing to
+# the dir containing the file outputed by function 'sf'
 function gt() {
 cd "$(dirname "$1")"
 }
 
-function rmallbutinput() {
-jobname=$1
-for extension in log out job gbw hess; do
-    if [ -f ${jobname}.${extension} ]; then
-        rm ${jobname}.${extension}
-    fi
-done
-if [ -f ${jobname}_opt.xyz ]; then
-    rm ${jobname}_opt.xyz
-fi
-}
 
-function ediff_opt() {
-START=$(grep "FINAL SINGLE POINT ENERGY" $1 | head -1 | cut -d"Y" -f2)
-STOP=$(grep "FINAL SINGLE POINT ENERGY" $1 | tail -1 | cut -d"Y" -f2)
-EDIFF=$(bc <<< "($STOP - $START)*627.509")
-echo "The energy difference is $EDIFF kcal/mol."
-}
-
+# Quickly open the scratch output file (.out) by giving the PID
 function viout() {
 OUTFILE=$(wd $1 | head -1)
 vi $OUTFILE
 }
 
+# Print a summary about a particular gaussian job:
+# gsum jobname.extension
 function gsum() {
 clear
 JOBNAME=$(echo $1 | cut -d"." -f1)
@@ -138,6 +137,8 @@ CHECKFILE=$(find /global/work/ambr/$1/* -iname "*.chk")
 chkchk $CHECKFILE
 }
 
+# Run the G16 submit script, but also show estimate of the requested CPU hours.
+# Also touch a file on the form "$jobfile.$pid"
 function g16() {
 pid=$(~/bin/g16.sh $1 $2 $3 | cut -d" " -f4)
 echo "Submitted batch job $pid"
@@ -153,12 +154,6 @@ HOURS=$(bc <<< "$2 * $3 * 16")
 echo "Requested number of CPU hours: $HOURS"
 }
 
-function g16_devel() {
-pid=$(~/bin/g16_devel.sh $1 | cut -d" " -f4)
-echo "Submitted batch job $pid"
-touch $1.$pid
-echo $pid > $1.$pid
-}
 
 function g16_bigmem() {
 pid=$(~/bin/g16_bigmem.sh $1 $2 $3 | cut -d" " -f4)
@@ -167,6 +162,7 @@ touch $1.$pid
 echo $pid > $1.$pid
 }
 
+# Show SLURM queue but also print key files below
 function qfil() {
 qcl
 IFS='
@@ -178,11 +174,13 @@ for i in $(squeue -hu ambr -S i); do
 done
 }
 
+# List all PID files in dir
 function lspid() {
 job=$(echo $1 | cut -d"." -f1)
 ls $job* | egrep '.*\.[0-9]{2,}'
 }
 
+# Do git pull in order to update the local git repo on stallo, and then make symlinks to bin
 function updatescripts() {
 DIR=/home/ambr/scripts_compchem
 CURR_DIR=$PWD
