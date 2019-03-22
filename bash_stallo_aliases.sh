ff5dd23e388e0071615d7603a6052616c488fb5e
Andersmb <anders.brakestad@gmail.com>
2019-03-22 12:37:46 +0100

    added alias for stripping extension from filename

--- bash_stallo_aliases.sh
+++ bash_stallo_aliases.sh
@@ -6,7 +6,7 @@ alias bashed="vim ~/.bashrc"
 alias killall="scancel -t PENDING -u ambr; scancel -t RUNNING -u ambr"
 alias vi="vim"
 alias bashsource="source $HOME/.bashrc"
-alias strip_ext="echo $1 | cut -d"," -f1"
+alias strip_ext="echo $1 | cut -d',' -f1"
 
 if [ $(echo $PWD | cut -d"/" -f2) == "cluster" ]; then
     alias queuegui="python $HOME/scripts_compchem/QueueGui-fram/queuegui.py"
