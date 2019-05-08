febe26c6040c0d6d64fc977e18b082ee72f8d496
Andersmb <anders.brakestad@gmail.com>
2019-05-08 18:25:20 +0200

    can now select the functionals of interest when getting hg data

--- stat_pol_analysis/functions.py
+++ stat_pol_analysis/functions.py
@@ -3,25 +3,25 @@ from pprint import pprint
 sys.path.append("/Users/{}/Documents/github/computational_chemistry".format(os.getenv("USER")))
 from MRChem import MrchemOut
 
-def get_HG_data(datafile):
+def get_HG_data(datafile, functionals=[]):
     """
     Collect data for the functionals we are interested in,
     and convert to yaml format. 
     Return the yaml data file name.
+    Give name of wanted functionals in list as kwarg to function
     """
     with open(datafile, "r") as ref:
         raw_data = ref.readlines()[:133]
     
-    raw_data = map(lambda x: x.split(";"), raw_data)
-    
-    # Use this list to filter the functionals we want
-    functionals = ["CCSD(T)", "Slater", "SPW92", "PBE", "PBE0", "TPSS", "B97", "B3LYP", "SCAN"]
+    raw_data = map(lambda x: x.split(","), raw_data)
     
     # Initialize the dict which will contains the data
     # and a dict for storing the column indeces of the 
     # functionals we are intersted in
     data = {}
     indeces = {}
+    # Convert to upper case
+    functionals = map(lambda x: x.upper(), functionals)
     for i, mol in enumerate(raw_data):
         # Determine the column index for the functionals we want
         if i == 0:
@@ -57,8 +57,17 @@ def decimal(s):
     """
     if "+" in s or "-" in s:
         return s[0] + s[1:2] + "." + s[2:]
-    elif "Zero" in s:
+    else:
         return "0"
+
+def stem(s):
+    """
+    Strip filename of its extension
+    """
+    if isinstance(s, str):
+        return s.split(".")[0]
+    else:
+        raise TypeError("You did not pass a string to the stem function")
 
 def get_pol_data(datadir):
     """
@@ -71,7 +80,7 @@ def get_pol_data(datadir):
     # Get all relevant output files in a list
     datafiles = os.path.join(os.getcwd(), datadir)
 
-    files = glob.glob("{}/*_*_*_*.out".format(datafiles))
+    files = glob.glob("{}/*.out".format(datafiles))
     molecules = set(map(lambda x: os.path.basename(x).split("_")[0], files))
     functionals = set(map(lambda x: os.path.basename(x).split("_")[1], files))
     
@@ -84,8 +93,8 @@ def get_pol_data(datadir):
     rawdata = {}
     rawdata["molecule"] =    [os.path.basename(f).split("_")[0] for f in files if os.path.basename(f).split("_")[0] not in error_molecules]
     rawdata["functional"] = [os.path.basename(f).split("_")[1] for f in files if os.path.basename(f).split("_")[0] not in error_molecules]
-    rawdata["direction"] =       [os.path.basename(f).split("_")[3].split(".")[0] for f in files if os.path.basename(f).split("_")[0] not in error_molecules]
-    rawdata["field"] = [decimal(os.path.basename(f).split("_")[2]) for f in files if os.path.basename(f).split("_")[0] not in error_molecules]
+    rawdata["direction"] =  [stem(os.path.basename(f)).split("_")[3] if len(stem(os.path.basename(f)).split("_")) == 4 else "non" for f in files if os.path.basename(f).split("_")[0] not in error_molecules]
+    rawdata["field"] = [decimal(stem(os.path.basename(f)).split("_")[2]) for f in files if os.path.basename(f).split("_")[0] not in error_molecules]
     rawdata["energy"] = []
     rawdata["u_norm"] = []
     rawdata["u_x"] = []
@@ -128,15 +137,14 @@ def get_pol_data(datadir):
         
     
     # now write raw data to CSV file using a useful pandas command
-    pandas.DataFrame(rawdata).to_csv("rawdata.csv")
-    return "mw_rawdata.csv"
+    pandas.DataFrame(rawdata).to_csv("mw_rawdata.csv")
+    return rawdata
    
 
-def make_nice_data(rawdatafile, fieldstrength=0.001, bohr_to_ang=1.8897162):
+def get_mw_pol_fdu(rawdatafile, fieldstrength=0.001, bohr_to_ang=1.8897162):
     """
     Extract relavant pol information from rawdata, and construct a dictionary to store
     the alpha_xx, alpha_yy, and alpha_zz values, for each molecule and each functional used.
-    Return the name of yaml data file.
 
     Parameters:
     rawdatafile       name of CSV file containing all raw data
@@ -205,4 +213,78 @@ def make_nice_data(rawdatafile, fieldstrength=0.001, bohr_to_ang=1.8897162):
         yaml.dump(data, f, default_flow_style=False)
     return data
     
+def get_mw_pol_fde(rawdatafile, fieldstrength=0.001, bohr_to_ang=1.8897162):
+    """
+    Extract relavant pol information from rawdata, and construct a dictionary to store
+    the alpha_xx, alpha_yy, and alpha_zz values, for each molecule and each functional used.
+
+    Parameters:
+    rawdatafile       name of CSV file containing all raw data
+    fieldstrength     the absolute value of the static electric field strength used
+    bohr_to_ang       Conversion factor for converting 1 Bohr to Angstrom
+    """
     
+    # String representation of the positive and negative fields used
+    # and the field directions
+    fields = ["+"+''.join(str(fieldstrength).split(".")), "-"+''.join(str(fieldstrength).split("."))]
+    directions = ["x", "y", "z"]
+    
+    with open(rawdatafile, "r") as raw:
+        jobs = raw.readlines()
+    
+    # identify the correct column indeces for data we need
+    index = {}
+    headers = ["energy", "molecule", "functional", "direction", "field", "u_x", "u_y", "u_z", "filename", "multiplicity"]
+    for i, raw_header in enumerate(jobs[0].split(",")):
+        if raw_header in headers:
+            index[raw_header] = i
+    
+    # Sort the raw data to make data extraction possible
+    jobs = sorted(map(lambda x: x.strip().split(","), jobs), key=operator.itemgetter(index["functional"], index["molecule"], index["direction"], index["field"]))
+    
+    # Get all unique molecules and functionals from rawdata
+    molecules = set([job[index["molecule"]] for i, job in enumerate(jobs) if i>0])
+    functionals = set([job[index["functional"]] for i, job in enumerate(jobs) if i>0])
+    
+    # Initialize data structure
+    data = {}
+    for mol in molecules:
+        data[mol] = {}
+        data[mol]["multiplicity"] = ""
+        for func in functionals:
+            data[mol][func] = {"diagonal": ["", "", ""],
+                               "mean": ""}
+            
+            # Now we need to add the correct information to the initialized lists
+            for field in fields:
+                for dirr in directions:
+                    # We will compare an ID to the job filename to make sure we get the correct data
+                    ID = "_".join([mol, func, field, dirr]) + ".out"
+
+                    # Avoid duplicates by making sure 'ID' is only the positive field jobs
+                    if "-0001" in ID:
+                        continue
+   
+                    # Now loop over the rawdata and compute the diagonal elements
+                    # We only need to get the multiplicity once
+                    # Due to the way the rawdata is sorted, we know that if the positive field job
+                    # is on line 'i', then the corresponding negative field job is on line 'i+1'
+                    # and that the zero-field job is at the "top" for each block of molecule jobs
+                    for i, job in enumerate(jobs):
+                        if ID == job[index["filename"]] and "_x" in ID:
+
+                            data[mol][func]["diagonal"][0] = -(float(jobs[i][index["energy"]]) + float(jobs[i+1][index["energy"]]) - 2*float(jobs[i-1][index["energy"]])) / fieldstrength**2 / bohr_to_ang**3
+                            data[mol]["multiplicity"] = int(jobs[i][index["multiplicity"]])
+
+                        elif ID == job[index["filename"]] and "_y" in ID:
+                            data[mol][func]["diagonal"][1] = -(float(jobs[i][index["energy"]]) + float(jobs[i+1][index["energy"]]) - 2*float(jobs[i-3][index["energy"]])) / fieldstrength**2 / bohr_to_ang**3
+                        elif ID == job[index["filename"]] and "_z" in ID:
+                            data[mol][func]["diagonal"][2] = -(float(jobs[i][index["energy"]]) + float(jobs[i+1][index["energy"]]) - 2*float(jobs[i-5][index["energy"]])) / fieldstrength**2 / bohr_to_ang**3
+            
+            # Now store the mean of the polarizability tensor diagonal
+            data[mol][func]["mean"] = sum(data[mol][func]["diagonal"]) / 3
+    # Now write the data to yaml file format
+    with open("mw_data.yaml", "w") as f:
+        yaml.dump(data, f, default_flow_style=False)
+    return data
+
