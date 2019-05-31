21eb91e72cd8b7ff52f857f7ceff4e1e1c8b5038
Andersmb <anders.brakestad@gmail.com>
2019-05-31 16:46:22 +0200

    removed functions not in use

--- bash_stallo_aliases.sh
+++ bash_stallo_aliases.sh
@@ -6,9 +6,3 @@ alias bashed="vim ~/.bashrc"
 alias killall="scancel -t PENDING -u ambr; scancel -t RUNNING -u ambr"
 alias vi="vim"
 alias bashsource="source $HOME/.bashrc"
-
-if [ $(echo $PWD | cut -d"/" -f2) == "cluster" ]; then
-    alias queuegui="python $HOME/scripts_compchem/QueueGui-fram/queuegui.py"
-else
-    alias queuegui="python $HOME/scripts_compchem/QueueGui/queuegui.py"
-fi
