c304a74348c98238bdb35a45bed07e6fb4e470c0
Andersmb <anders.brakestad@gmail.com>
2019-05-31 16:32:32 +0200

    moved to own rep

--- stat_pol_analysis/test.py
+++
@@ -1,7 +0,0 @@
-import functions as f
-from pprint import pprint
-a = f.get_mw_pol_fde("orca_rawdata.csv", fieldstrength=0.01)
-b = f.get_HG_data("hg_data.csv", ["pbe"])
-
-for mol in a.keys():
-    print(mol, a[mol]["pbe"]["mean"], b[mol]["pbe"]["mean"])
