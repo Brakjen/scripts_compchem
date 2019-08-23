cd25d42f73b1a520d12da824c64b8aa1fb20c605
Andersmb <anders.brakestad@gmail.com>
2019-08-23 16:11:25 +0200

    cleaned up code

--- Gaussian.py
+++ Gaussian.py
@@ -19,24 +19,24 @@ class GaussianOut(object):
             return result
         return timed
 
-    @timeit
+    #@timeit
     def __repr__(self):
         return "<GaussianOut(filename={})>".format(self.filename)
 
-    @timeit
+    #@timeit
     def content(self):
         """Return a generator that iterates over lines in the file, one by one."""
         with open(self.filename, "r") as f:
             while True:
                 yield f.next()
     
-    @timeit
+    #@timeit
     def source(self):
         """Return the file content as a string."""
         with open(self.filename, "r") as f:
             return f.read()
     
-    @timeit
+    #@timeit
     def normaltermination(self):
         """Evaluate whether the job
         terminated normally, and return a Boolean:
@@ -45,32 +45,32 @@ class GaussianOut(object):
             return True
         return False
     
-    @timeit
+    #@timeit
     def scf_energy(self):
         """Return a list of floats containing the optimized SCF energies"""
         energies = filter(lambda x: x.strip().startswith("SCF Done:"), self.content())
         return map(lambda x: float(x.split()[4]), energies)
 
-    @timeit
+    #@timeit
     def no_scfcycles(self):
         """Return a list of floats containing the number of SCF iterations needed for converging each geom step"""
         cycles = filter(lambda x: x.strip().startswith("SCF Done:"), self.content())
         return map(int, map(lambda x: x.strip().split()[7], cycles))
 
-    @timeit
+    #@timeit
     def walltime(self):
         """Return the total walltime for the job (float) in seconds"""
         w = filter(lambda x: x.strip().startswith("Elapsed time:"), self.content())[0].strip().split()
         return float(w[2])*24*60*60 + float(w[4])*60*60 + float(w[6])*60 + float(w[8])
 
-    @timeit
+    #@timeit
     def no_atoms(self):
         """Return the number of atoms of the system (integer)"""
         for line in self.content():
             if line.strip().startswith("NAtoms="):
                 return int(line.strip().split()[1])
 
-    @timeit
+    #@timeit
     def geometry_trajectory(self):
         """Return list of all geometry steps from a geometry optimization. The last step is the optimized geometry"""
         natoms = self.no_atoms()
@@ -112,12 +112,12 @@ class GaussianOut(object):
         # We discard the last geometry because it will be a duplicate
         return traj[:-1]
     
-    @timeit
+    #@timeit
     def no_geomcycles(self):
         """Return the number of geometry cycles needed for convergence. Return an integer."""
         return len(list(self.geometry_trajectory()))
     
-    @timeit
+    #@timeit
     def no_basisfunctions(self):
         """Return the number of basis functions (integer)."""
         nbasis = None
@@ -127,73 +127,77 @@ class GaussianOut(object):
                 break
         return nbasis
 
-    @timeit
+    #@timeit
     def maxforce(self):
         """Return a list of floats containing all Max Forces for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("Maximum Force"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
 
-    @timeit
+    #@timeit
     def rmsforce(self):
         """Return a list of floats containing all RMS Forces for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("RMS Force"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
 
-    @timeit
+    #@timeit
     def maxstep(self):
         """Return a list of floats containing all Max Steps for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("Maximum Displacement"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
 
-    @timeit
+    #@timeit
     def rmsstep(self):
         """Return a list of floats containing all RMS Steps for each geometry iteration"""
         l = filter(lambda x: ' '.join(x.split()).startswith("RMS Displacement"), self.content())
         l = map(lambda x: x.split()[2], l)
         return map(float, l)
     
-    @timeit
+    #@timeit
     def tol_maxforce(self):
         """Return the Max Force convergence tolerance as float"""
         t = None
         for line in self.content():
             if line.strip().startswith("Maximum Force"):
-                t = float(line.strip().split()[3])
-                break
-        return t
+                t = line.strip().split()[3]
+                if "D" in t:
+                    return float(t.replace("D", "e"))
+                return float(t)
 
-    @timeit
+    #@timeit
     def tol_rmsforce(self):
         """Return the RMSD Force convergence tolerance as float"""
         t = None
         for line in self.content():
             if ' '.join(line.strip().split()).startswith("RMS Force"):
-                t = float(line.strip().split()[3])
-                break
-        return t
+                t = line.strip().split()[3]
+                if "D" in t:
+                    return float(t.replace("D", "e"))
+                return float(t)
 
-    @timeit
+    #@timeit
     def tol_maxstep(self):
         """Return the Max Step convergence tolerance as float"""
         t = None
         for line in self.content():
             if line.strip().startswith("Maximum Displacement"):
-                t = float(line.strip().split()[3])
-                break
-        return t
+                t = line.strip().split()[3]
+                if "D" in t: # Gaussian uses the D in its scientific notation of floats. stupid!
+                    return float(t.replace("D", "e"))
+                return float(t)
 
-    @timeit
+    #@timeit
     def tol_rmsstep(self):
         """Return the RMSD Step convergence tolerance as float"""
         t = None
         for line in self.content():
             if ' '.join(line.strip().split()).startswith("RMS Displacement"):
-                t = float(line.strip().split()[3])
-                break
-        return t
+                t = line.strip().split()[3]
+                if "D" in t:
+                    return float(t.replace("D", "e"))
+                return float(t)
 
 
 # This class may not be useful for anything.....
